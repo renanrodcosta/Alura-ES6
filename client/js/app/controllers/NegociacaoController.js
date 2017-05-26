b7ec36044c6c089040e805017b9d241b7dda58d3
Renan Costa <renan.costa@lambada3.com.br>
2017-05-25 21:56:40 -0300

    Obtendo os input da view

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,6 +2,15 @@ class NegociacaoController{
 
     adicionar(event){
         event.preventDefault();
-        alert("chamou");
+        
+        let $ = document.querySelector.bind(document);
+
+        let inputData = $("#data");
+        let inputQuantidade = $("#quantidade");
+        let inputValor = $("#valor");
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
     }
 }
