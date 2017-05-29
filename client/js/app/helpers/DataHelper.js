b814726bd7b5919a5f485f30adb7a26d62372c77
Renan Costa <renan.costa@lambada3.com.br>
2017-05-28 22:53:29 -0300

    Formatando string com intepolation e validando formato de data com regex

--- client/js/app/helpers/DataHelper.js
+++ client/js/app/helpers/DataHelper.js
@@ -5,10 +5,15 @@ class DataHelper{
     }
 
     static dataParaTexto(data){
-        return data.getDate() + "/" + (data.getMonth() + 1) + "/" + data.getFullYear()
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`
     }
 
     static textoParaData(texto){
+
+        let formatoCorreto = !/\d{4}-\d{2}-\d{2}/.test(texto)
+
+        if(formatoCorreto) throw new Error("Deve estar no formato aaaa-mm-dd.")
+
         return new Date(...texto.split("-").map((item, index) => item - index % 2 ))
     }
 }
