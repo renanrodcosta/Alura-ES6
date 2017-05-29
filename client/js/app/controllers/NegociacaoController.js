f0cc1bbb469b9aa809ebee3210a9e0a06d193b9a
Renan Costa <renan.costa@lambada3.com.br>
2017-05-28 23:23:45 -0300

    Criando Lista de negocioações

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,6 +6,7 @@ class NegociacaoController{
         this._inputData = $("#data")
         this._inputQuantidade = $("#quantidade")
         this._inputValor = $("#valor")
+        this._listaNegociacoes = new ListaNegociacoes()
     }
 
     adicionar(event){
@@ -15,7 +16,23 @@ class NegociacaoController{
                                         this._inputQuantidade.value,
                                         this._inputValor.value)
 
-        console.log(negociacao)
-        console.log(DataHelper.dataParaTexto(negociacao.data))
+        this._listaNegociacoes.adiciona(this._criaNegociacao())
+        this._limpaFormulario()
+
+        console.log(this._listaNegociacoes.negociacoes)
+        
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(DataHelper.textoParaData(this._inputData.value),
+                                        this._inputQuantidade.value,
+                                        this._inputValor.value)
+    }
+
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 }
