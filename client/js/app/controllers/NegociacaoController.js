61373e44f520251c72b8d6d8f6d7851470808a70
Renan Costa <renan.costa@lambada3.com.br>
2017-05-28 21:49:52 -0300

    Criando negociação utilizando spread operator e arraw function

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,17 +3,23 @@ class NegociacaoController{
     constructor() {
         let $ = document.querySelector.bind(document);
 
-        this.inputData = $("#data");
-        this.inputQuantidade = $("#quantidade");
-        this.inputValor = $("#valor");
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
     }
-
 
     adicionar(event){
         event.preventDefault();    
 
-        console.log(this.inputData.value);
-        console.log(this.inputQuantidade.value);
-        console.log(this.inputValor.value);
+        let data = new Date(...this._inputData.value.split("-")
+                            .map((item, index) => item - index % 2 ))
+
+        console.log(data)
+
+        let negociacao = new Negociacao(data,
+                                        this._inputQuantidade.value,
+                                        this._inputValor.value)
+
+        console.log(negociacao)
     }
 }
