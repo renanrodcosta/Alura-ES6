f150df80622b1db1d450a37db23f9c240c11b9c5
Renan Costa <renan.costa@lambada3.com.br>
2017-05-28 22:29:42 -0300

    Criando classe DateHelper

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,25 +1,22 @@
 class NegociacaoController{
 
     constructor() {
-        let $ = document.querySelector.bind(document);
+        let $ = document.querySelector.bind(document)
 
-        this._inputData = $("#data");
-        this._inputQuantidade = $("#quantidade");
-        this._inputValor = $("#valor");
+        this._inputData = $("#data")
+        this._inputQuantidade = $("#quantidade")
+        this._inputValor = $("#valor")
     }
 
     adicionar(event){
-        event.preventDefault();    
+        event.preventDefault()    
 
-        let data = new Date(...this._inputData.value.split("-")
-                            .map((item, index) => item - index % 2 ))
-
-        console.log(data)
-
-        let negociacao = new Negociacao(data,
+        let dataHelper = new DataHelper()
+        let negociacao = new Negociacao(dataHelper.textoParaData(this._inputData.value),
                                         this._inputQuantidade.value,
                                         this._inputValor.value)
 
         console.log(negociacao)
+        console.log(dataHelper.dataParaTexto(negociacao.data))
     }
 }
---
+++ client/js/app/helpers/DataHelper.js
@@ -0,0 +1,12 @@
+class DataHelper{
+    
+    dataParaTexto(data){
+        return data.getDate() + "/" + (data.getMonth() + 1) + "/" + data.getFullYear()
+    }
+
+    textoParaData(texto){
+        return new Date(...texto.split("-").map((item, index) => item - index % 2 ))
+    }
+
+
+}
