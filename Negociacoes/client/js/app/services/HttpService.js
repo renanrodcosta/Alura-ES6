17c105d692c24042a94cb977ce5ea742b8cbb309
Renan Costa <renan.costa@lambada3.com.br>
2017-06-23 22:04:13 -0300

    Criando fetch de metodo post

--- Negociacoes/client/js/app/services/HttpService.js
+++ Negociacoes/client/js/app/services/HttpService.js
@@ -1,27 +1,19 @@
 class HttpService{
 
-    get (url) {
+    get(url) {
         return fetch(url)
             .then(response => this._handlerErrors(response))
             .then(response => response.json())
     }
 
     post(url, dado) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest()
-            xhr.open("POST", url, true)
-            xhr.setRequestHeader("Content-type", "application/json")
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText))
-                    } else {
-                        reject(xhr.responseText)
-                    }
-                }
-            };
-            xhr.send(JSON.stringify(dado))
-        });
+
+        return fetch(url, {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+        .then(res => this._handleErrors(res))
     }
 
     _handlerErrors(response){
